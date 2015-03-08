1f48e1ff45db7c259772aaf95046df75990ef095
Pascal Hertleif <killercup@gmail.com>
2015-03-08 13:21:32 +0100

    Add update script

---
+++ update.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+
+BASE=`pwd`
+GH_PAGES="${BASE}/.git/gh-pages/"
+
+has_changes () {
+  ! git diff --quiet
+}
+
+git submodule update --remote rust \
+&& bitrust > "${GH_PAGES}log.json" \
+&& cd $GH_PAGES \
+&& has_changes \
+&& git commit -a -m "Manual update" \
+&& git push \
+|| echo "No changes";
+
