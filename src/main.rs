e073318a054b53815633a099f71170c1e525a01c
navigaid <navigaid@gmail.com>
2020-10-05 02:48:15 +0800

    cargo fmt

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,7 @@ extern crate git2;
 extern crate serde;
 extern crate serde_json;
 
-use std::env::args;
-use git2::{Repository, Oid};
-use git2::Error as GitError;
+use git2::{Oid, Repository, Revwalk, Error, Sort};
 use serde::{Serialize, Deserialize};
 
 /// Simple commit, struct determines JSON output
@@ -17,42 +15,44 @@ struct Commit {
     /// Commit creationg date as UNIX timestamp
     date: i64,
     /// Commit message
-    message: String
+    message: String,
 }
 
 /// Retrieve commits matching query from repsitory.
-fn fetch_commits(repo: &str, rev: &str, query: &str) -> Result<Vec<Commit>, GitError> {
+fn fetch_commits(repo: &str, rev: &str, query: &str) -> Result<Vec<Commit>, Error> {
     let repo: Repository = Repository::open(repo)?;
-    let rev: Option<Oid> = Oid::from_str(&rev).ok();
-    let mut revs = repo.revwalk()?;
-    match rev {
-        Some(commit_id) => revs.push(commit_id)?,
-        _ => revs.push_head()?,
+    let mut walker: Revwalk = repo.revwalk()?;
+
+    // At least one commit must be pushed onto the walker before a walk can be started.
+    match Oid::from_str(rev).ok() {
+        Some(commit_id) => walker.push(commit_id)?,
+        _ => walker.push_head()?,
     }
 
-    revs.set_sorting(git2::Sort::TOPOLOGICAL | git2::Sort::TIME)?;
+    walker.set_sorting(Sort::TOPOLOGICAL | Sort::TIME)?;
 
-    let commits = revs
-    .filter_map(|commit_id| { repo.find_commit(commit_id.unwrap()).ok() })
-    .filter(|commit| { commit.message().map(|m| m.contains(query)).unwrap_or(false) })
-    .map(|commit| {
-        let c = Commit {
-            hash: commit.id().to_string(),
-            author: commit.author().name()
-                .or(commit.author().email())
-                .unwrap_or("Some dude").trim().to_string(),
-            date: commit.time().seconds(),
-            message: commit.message().unwrap_or("").trim().to_string()
-        };
-        c
-    })
-    .collect();
+    let commits = walker.filter_map(|commit_id| repo.find_commit(commit_id.unwrap()).ok())
+        .filter(|commit| commit.message().map(|m| m.contains(query)).unwrap_or(false))
+        .map(|commit| {
+            Commit {
+                hash: commit.id().to_string(),
+                author: commit.author()
+                    .name()
+                    .or(commit.author().email())
+                    .unwrap_or("Some dude")
+                    .trim()
+                    .to_string(),
+                date: commit.time().seconds(),
+                message: commit.message().unwrap_or("").trim().to_string(),
+            }
+        })
+        .collect();
 
     Ok(commits)
 }
 
-fn main(){
-    let revision: String = args().nth(1).unwrap_or(String::new());
-    let commits = fetch_commits("rust", &revision, "[breaking-change]").unwrap();
+fn main() {
+    let revision = std::env::args().nth(1).unwrap_or(String::new());
+    let commits = fetch_commits("./rust", revision.trim(), "[breaking-change]").unwrap();
     println!("{}", serde_json::to_string_pretty(&commits).unwrap());
 }
